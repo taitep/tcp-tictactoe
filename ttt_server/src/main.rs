4a29be1fafb7128f3e0312e1dc5bb213e7e962e9
taitep <taitep@taitep.se>
2024-06-24 14:08:36 +0200

    start implementing client accepting in the server

--- ttt_server/src/main.rs
+++ ttt_server/src/main.rs
@@ -1,27 +1,58 @@
-use std::{net::TcpListener, sync::mpsc::{Receiver, Sender, channel}};
+use std::{
+    net::TcpListener,
+    sync::mpsc::{Receiver, Sender, channel},
+    thread
+};
 
-struct BidirectionalChannelEnd<T> {
-    rx: Receiver<T>,
-    tx: Sender<T>,
+use anyhow::Result;
+
+struct BidirectionalChannelEnd<S, R> {
+    rx: Receiver<R>,
+    tx: Sender<S>,
 }
 
-impl<T> BidirectionalChannelEnd<T> {
-    fn new_pair() -> (Self, Self) {
-        let (txa, rxa) = channel::<T>();
-        let (txb, rxb) = channel::<T>();
+impl<A, B> BidirectionalChannelEnd<A, B> {
+    fn new_pair() -> (BidirectionalChannelEnd<A, B>, BidirectionalChannelEnd<B, A>) {
+        let (txa, rxa) = channel::<A>();
+        let (txb, rxb) = channel::<B>();
         (
-            Self {
-                rx: rxa,
-                tx: txb,
-            },
-            Self {
+            BidirectionalChannelEnd {
                 rx: rxb,
                 tx: txa,
+            },
+            BidirectionalChannelEnd {
+                rx: rxa,
+                tx: txb,
             },
         )
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:7777");
+fn game_manager(client_receiver: Receiver<BidirectionalChannelEnd<(), ()>>) {
+
+}
+
+fn handle_client(channel: BidirectionalChannelEnd<(), ()>) {
+
+}
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:7777")?;
+    let (send_client, get_client) = channel::<BidirectionalChannelEnd<(), ()>>();
+
+    thread::spawn(move || {
+        game_manager(get_client);
+    });
+
+    loop {
+        let (client, addr) = listener.accept()?;
+        println!("Client {addr} has connected, adding to queue...");
+
+        let (server_channel_end, client_channel_end) = BidirectionalChannelEnd::<(), ()>::new_pair();
+        send_client.send(server_channel_end).expect("Unhandlable internal error");
+
+        thread::spawn(move || {
+            handle_client(client_channel_end);
+        });
+    }
 }
