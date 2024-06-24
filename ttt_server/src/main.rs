dfeddbfabb7ac35eb33833e5442ee5145d803e56
taitep <taitep@taitep.se>
2024-06-24 13:29:02 +0200

    Implement 'BidirectionalChannelEnd'

--- ttt_server/src/main.rs
+++ ttt_server/src/main.rs
@@ -1,4 +1,26 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, sync::mpsc::{Receiver, Sender, channel}};
+
+struct BidirectionalChannelEnd<T> {
+    rx: Receiver<T>,
+    tx: Sender<T>,
+}
+
+impl<T> BidirectionalChannelEnd<T> {
+    fn new_pair() -> (Self, Self) {
+        let (txa, rxa) = channel::<T>();
+        let (txb, rxb) = channel::<T>();
+        (
+            Self {
+                rx: rxa,
+                tx: txb,
+            },
+            Self {
+                rx: rxb,
+                tx: txa,
+            },
+        )
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7777");
